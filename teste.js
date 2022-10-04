14b29bb0a1578cdf10e40dca56a4bed7243fad7e
J0NGS <jnetogoncalo@gmail.com>
2022-10-04 15:11:13 -0300

    FUNC TRANSPOSER OK

--- teste.js
+++ teste.js
@@ -5,12 +5,20 @@ import { Matriz } from './Matriz.mjs';
 let A = new Matriz(2,1);
 let B = new Matriz(2,1);
 A.randomize();
+console.log("MATRIZ A");
 A.print();
+console.log("MATRIZ B");
 B.randomize();
 B.print();
 
 let C = Matriz.hadamaard(A,B);
+console.log("HADAMAARD A X B");
 C.print();
 
 C = Matriz.multi_escalar(A,10);
+console.log("MULTI ESCALAR A X 10");
+C.print();
+
+C = Matriz.transposer(A);
+console.log("TRANSPOSTA A");
 C.print();
