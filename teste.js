62f3b3c3411b3fbe0f0aea60bdb21ef9a38a94fc
J0NGS <jnetogoncalo@gmail.com>
2022-10-04 15:12:49 -0300

    FUNC SUB OK

--- teste.js
+++ teste.js
@@ -4,11 +4,13 @@ import { Matriz } from './Matriz.mjs';
 
 let A = new Matriz(2,1);
 let B = new Matriz(2,1);
+
 A.randomize();
 console.log("MATRIZ A");
 A.print();
-console.log("MATRIZ B");
+
 B.randomize();
+console.log("MATRIZ B");
 B.print();
 
 let C = Matriz.hadamaard(A,B);
@@ -21,4 +23,8 @@ C.print();
 
 C = Matriz.transposer(A);
 console.log("TRANSPOSTA A");
+C.print();
+
+C = Matriz.sub(A,B);
+console.log("SUBTRAÇÃO A - B");
 C.print();
