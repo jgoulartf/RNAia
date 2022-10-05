e2715971e46f7a59f97f672cc06448012a89bc01
João Goulart <joao123goulart@gmail.com>
2022-10-04 23:45:09 -0300

    segundo if ok

--- teste.js
+++ teste.js
@@ -1,56 +1,113 @@
 import{RNA} from './RNA.mjs';
 import { Matriz } from './Matriz.mjs';
-var rna = new RNA(2,3,1);
-var dataset;
+
+let dataset = {
+  numbers : [
+    [ 0, 1, 1, 0,
+      1, 0, 0, 1,
+      1, 0, 0, 1,       // 0
+      1, 0, 0, 1,
+      0, 1, 1, 0
+    ],
+    [ 0, 1, 0, 0,
+      1, 1, 0, 0,
+      0, 1, 0, 0,       // 1
+      0, 1, 0, 0,
+      1, 1, 1, 0
+    ],
+    [ 0, 1, 1, 0,
+      1, 0, 0, 1,
+      0, 0, 1, 0,       // 2
+      0, 1, 0, 0,
+      1, 1, 1, 1
+    ],
+    [ 1, 1, 1, 0,
+      0, 0, 0, 1,
+      0, 0, 1, 0,       // 3
+      0, 0, 0, 1,
+      1, 1, 1, 0
+    ],
+    [ 1, 0, 1, 0,
+      1, 0, 1, 0,
+      1, 1, 1, 1,       // 4
+      0, 0, 1, 0,
+      0, 0, 1, 0
+    ],
+    [ 1, 1, 1, 1,
+      1, 0, 0, 0,
+      1, 1, 1, 0,       // 5
+      0, 0, 0, 1,
+      1, 1, 1, 0
+    ],
+    [ 0, 1, 1, 1,
+      1, 0, 0, 0,
+      1, 1, 1, 0,       // 6
+      1, 0, 0, 1,
+      0, 1, 1, 0
+    ],
+    [ 1, 1, 1, 1,
+      0, 0, 0, 1,
+      0, 0, 1, 0,       // 7
+      0, 1, 0, 0,
+      0, 1, 0, 0
+    ],
+    [ 0, 1, 1, 0,
+      1, 0, 0, 1,
+      0, 1, 1, 0,       // 8
+      1, 0, 0, 1,
+      0, 1, 1, 0
+    ],
+    [ 0, 1, 1, 0,
+      1, 0, 0, 1,
+      0, 1, 1, 1,       // 9
+      0, 0, 0, 1,
+      1, 1, 1, 1
+    ],
+  ],
+  names   : ["Goulart", "Goncalo", "Francisco", "Jose", "Maria"],
+  outputs : [ 6, 6, 8, 6, 7],
+}
 
 //XOR PROBLEM
-dataset = {
-    inputs:
+let datasetXor = {
+  inputs:
     [[1, 1],
-    [1, 0],
-    [0, 1],
-    [0, 0]],
-    outputs:
+      [1, 0],
+      [0, 1],
+      [0, 0]],
+  outputs:
     [[0],
-    [1],
-    [1],
-    [0]]
+      [1],
+      [1],
+      [0]]
 }
-
+let rna = new RNA(2, 3, 1);
 var train = true;
-if (train) {
-    for (var i = 0; i < 10000; i++) {
-        var index = Math.floor(Math.random(4));
-        rna.train(dataset.inputs[index], dataset.outputs[index]);
-    }
-    if (rna.predict([0, 0])[0] < 0.04 && rna.predict([1, 0])[0] > 0.98) {
-        console.log("entrou no segundo if")
-        train = false;
+
+// testando se rede ta funcionando
+function training() {
+  console.log("Iniciando treino...");
+  if(train) {
+     for(let i = 0; i < 100; i++) {
+      let index = Math.floor(Math.random());
+      rna.train(datasetXor.inputs[index], datasetXor.outputs[index])
     }
 
+    if(rna.predict([0, 0])[0] < 0.04 ||  rna.predict([1, 0])[0] > 0.98){
+      train = false;
+      console.log("Treino finalizado...\n");
+    }
+
+  }
 }
-/*let B = new Matriz(2,1);
 
-A.randomize();
-console.log("MATRIZ A");
-A.print();
+function testing() {
+  console.log("ESPERADO --> [1, 1] --> [0]");
+  console.log("OBTIDO   -->", rna.predict([1, 1]));
 
-B.randomize();
-console.log("MATRIZ B");
-B.print();
+  console.log("ESPERADO --> [1, 0] --> [1]");
+  console.log("OBTIDO   -->", rna.predict([1, 0]));
+}
 
-let C = Matriz.hadamaard(A,B);
-console.log("HADAMAARD A X B");
-C.print();
-
-C = Matriz.multi_escalar(A,10);
-console.log("MULTI ESCALAR A X 10");
-C.print();
-
-C = Matriz.transposer(A);
-console.log("TRANSPOSTA A");
-C.print();
-
-C = Matriz.sub(A,B);
-console.log("SUBTRAÇÃO A - B");
-C.print();*/
+training();
+testing();
